078d9f91a6da571521cb239d57d9709773adca8a
Juan Cruz <69728320+juance20@users.noreply.github.com>
2023-05-27 21:36:46 -0300

    Update wallpapers.sh

--- .config/waybar/wallpapers.sh
+++ .config/waybar/wallpapers.sh
@@ -15,6 +15,15 @@ if [ "$selection" = "change" ]; then
 		new_wallpaper="wallpaper3.png"
 		swww img ~/Pictures/$new_wallpaper --transition-type center
 	elif [ "$current_wallpaper" = "wallpaper3.png" ]; then
+		new_wallpaper="wallpaper4.png"
+		swww img ~/Pictures/$new_wallpaper --transition-type center
+	elif [ "$current_wallpaper" = "wallpaper4.png" ]; then
+		new_wallpaper="wallpaper5.png"
+		swww img ~/Pictures/$new_wallpaper --transition-type center
+	elif [ "$current_wallpaper" = "wallpaper5.png" ]; then
+		new_wallpaper="girl.gif"
+		swww img ~/Pictures/$new_wallpaper --transition-type center
+	elif [ "$current_wallpaper" = "girl.gif" ]; then
 		new_wallpaper="wallpaper1.png"
 		swww img ~/Pictures/$new_wallpaper --transition-type center
 	else
